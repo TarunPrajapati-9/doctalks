b9d74c9a070fe1d3bc8fb5242bc9dc4df85efa24
Tarun Prajapati <tarun.prajapati912@gmail.com>
2024-02-28 20:10:03 +0530

    Bug fixing

--- user/src/Component/NavBar.jsx
+++ user/src/Component/NavBar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  let navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.clear();
+    navigate("/signIn");
+  };
   return (
     <div className="navbar bg-[#B7EFC5] bg-opacity-35">
       <Link to="/" className="flex-1">
@@ -17,11 +24,15 @@ const NavBar = () => {
           <li>
             <Link to="/chatbot">Chat with AI ✨</Link>
           </li>
-          <li>
-            <Link to="/signin" className="btn btn-sm btn-ghost">
-              Logout
-            </Link>
-          </li>
+          {localStorage.getItem("user_token") && (
+            <>
+              <li key="logout">
+                <button type="button" onClick={handleLogOut}>
+                  Logout&#8594;
+                </button>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
