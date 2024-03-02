ed2952d5a4879315e335b74abb56393799564efd
Tarun Prajapati <tarun.prajapati912@gmail.com>
2024-03-02 09:24:55 +0530

    Listing Description Added in MarkDown

--- user/src/Component/SessionCard.jsx
+++ user/src/Component/SessionCard.jsx
@@ -1,16 +1,25 @@
 import { useContext, useEffect } from "react";
 import UserContext from "../Context/UserContext";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-export default function EventCard({ duration, title, price, doctorname }) {
+export default function EventCard({
+  duration,
+  title,
+  price,
+  doctorname,
+  description,
+}) {
   const { openModal, setsessionDetail } = useContext(UserContext);
 
-  const handleOnClick = (duration, title, price, doctorname) => {
+  const handleOnClick = (duration, title, price, doctorname, description) => {
     openModal(),
       setsessionDetail({
         duration: duration,
         title: title,
         price: 0,
         doctorname: doctorname,
+        description: description,
       });
   };
   return (
@@ -23,14 +32,18 @@ export default function EventCard({ duration, title, price, doctorname }) {
           <span className="text-3xl font-semibold text-blue-700">Book?</span>
           <br></br>
         </h1>
-
+        <ReactMarkdown remarkPlugins={[remarkGfm]} className="prose">
+          {description}
+        </ReactMarkdown>
         <div className="flex flex-col justify-center w-full text-center mt-4 gap-6 sm:flex-row text-lg font-semibold">
           <a className="flex flex-row justify-center  py-2 h-12 w-32 border border-blue-700 rounded-lg hover:shadow-2xl hover:text-blue-700 shadow-lg">
             {price}
           </a>
           <a
-            onClick={() => handleOnClick(duration, title, price, doctorname)}
-            className="flex flex-row justify-center bg-blue-600 h-12 w-32 sm:w-32 py-2 rounded-lg text-white hover:bg-blue-700 shadow-lg"
+            onClick={() =>
+              handleOnClick(duration, title, price, doctorname, description)
+            }
+            className="flex flex-row justify-center bg-blue-600 h-12 w-32 sm:w-32 py-2 rounded-lg text-white hover:bg-blue-700 shadow-lg cursor-pointer"
           >
             Book Now
           </a>
--- user/src/app/Booking.jsx
+++ user/src/app/Booking.jsx
@@ -7,10 +7,9 @@ import { listing } from "../utils/dataPoster";
 import { useParams } from "react-router-dom";
 
 const Booking = () => {
-
   const [date, setdate] = useState({
     startDate: null,
-    endDate: null
+    endDate: null,
   });
 
   const handleValueChange = (newValue) => {
@@ -19,7 +18,7 @@ const Booking = () => {
     // console.log(error.response.data.message);
   };
 
-  // 
+  //
   const { docId } = useParams();
 
   const { mutate, data, isPending, isError, error } = useMutation({
@@ -28,44 +27,35 @@ const Booking = () => {
 
   const [hasLoaded, setHasLoaded] = useState(false);
 
-
-
   useEffect(() => {
-
     if (!hasLoaded) {
       mutate({ doctorID: docId });
       setHasLoaded(true);
     }
-
   });
-
-
 
   const getTodayListing = (event) => {
     event.preventDefault(); // Pr
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate()); // Incrementing current date by 1 to get tomorrow's date
     const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDate().toString().padStart(2, '0');
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = currentDate.getDate().toString().padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
 
     mutate({ doctorID: docId, date: formattedDate });
     // console.log(error.response.data.message);
-
-  }
+  };
   const getTommmorowListing = (event) => {
     event.preventDefault(); // Pr
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() + 1); // Incrementing current date by 1 to get tomorrow's date
     const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDate().toString().padStart(2, '0');
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = currentDate.getDate().toString().padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
     mutate({ doctorID: docId, date: formattedDate });
-  }
-
-
+  };
 
   // const { SessionInfo } = UserStore();
   const { openModal } = useContext(UserContext);
@@ -100,23 +90,26 @@ const Booking = () => {
           <div className="flex flex-col items-center justify-center gap-5 mt-6 md:flex-row">
             <div className="flex flex-row gap-8">
               <div>
-                <button onClick={
-                  getTodayListing}>
+                <button onClick={getTodayListing}>
                   <a
                     className="inline-block w-auto min-w-[150px] px-6 py-4 text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-indigo-500 to-indigo-400 hover:bg-gradient-to-b   hover:shadow-2xl hover:shadow-indigo-400 hover:-translate-y-px "
-                    href="">Today
+                    href=""
+                    disabled={isPending}
+                  >
+                    Today
                   </a>
                 </button>
               </div>
 
-              <div role="button" onClick={
-                getTommmorowListing}>
+              <div role="button" onClick={getTommmorowListing}>
                 <a
                   className="inline-block w-auto min-w-[150px] px-6 py-4 text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-indigo-500 to-indigo-400 hover:bg-gradient-to-b   hover:shadow-2xl hover:shadow-indigo-400 hover:-translate-y-px "
-                  href="">Tomorrow
+                  href=""
+                  disabled={isPending}
+                >
+                  Tomorrow
                 </a>
               </div>
-
             </div>
             <Datepicker
               primaryColor={"while"}
@@ -124,55 +117,57 @@ const Booking = () => {
               asSingle={true}
               value={date}
               onChange={handleValueChange}
-              style={{ color: 'black', backgroundColor: 'white' }}
+              disabled={isPending}
+              style={{ color: "black", backgroundColor: "white" }}
             />
-
           </div>
         </div>
 
-
         <div className="flex flex-row flex-wrap gap-10 border justify-center md:justify-start p-4 ">
-
           {isError && <div>{error.response.data.message}</div>}
 
-
-          {(data?.listings && (data?.listings ?? []).length > 0) && data?.listings.map((listing) => (
-            <SessionCard
-              key={listing._id} // Using _id as the key for each SessionCard
-              title={listing.title}
-              price={listing.price + "rs"} // Concatenate "rs" to the price
-              duration={`${listing.avg_duration} minutes`} // Assuming avg_duration is in minutes
-              doctorname={listing.doctor_id} // You might need to fetch the doctor name from another source based on the doctor_id
-            />
-          ))}
-
-
-
+          {data?.listings &&
+            (data?.listings ?? []).length > 0 &&
+            data?.listings.map((listing) => (
+              <SessionCard
+                key={listing._id} // Using _id as the key for each SessionCard
+                title={listing.title}
+                price={listing.price + "rs"} // Concatenate "rs" to the price
+                duration={`${listing.avg_duration} minutes`} // Assuming avg_duration is in minutes
+                doctorname={listing.doctor_id}
+                description={listing.description} // You might need to fetch the doctor name from another source based on the doctor_id
+              />
+            ))}
         </div>
 
         {/* Open the modal using document.getElementById('ID').showModal() method */}
 
         <dialog id="my_modal_1" className="modal">
           <div className="modal-box">
-            <h3 className="font-bold text-lg">Are you sure for purchasing this Session in 99!</h3>
+            <h3 className="font-bold text-lg">
+              Are you sure for purchasing this Session in 99!
+            </h3>
             <p className="py-4">After payment it is not refundable </p>
             <div className="modal-action">
-              <form method="dialog" >
+              <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
                 <button className="btn mr-2">No</button>
                 {/* <button className="btn ml-2 " onClick={() => { openModal(); makePayment() }}>YEs</button> */}
-                <button className="btn ml-2 " onClick={() => { openModal(); }}>YEs</button>
+                <button
+                  className="btn ml-2 "
+                  onClick={() => {
+                    openModal();
+                  }}
+                >
+                  Yes
+                </button>
               </form>
             </div>
           </div>
         </dialog>
-
       </div>
     </>
   );
 };
 
 export default Booking;
-
-
-
